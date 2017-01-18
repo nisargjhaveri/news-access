67a7667cacc2f439044b25a51edef3f0f58deb93
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2017-01-18 20:31:40 +0530

    Minor fixes/refactor

--- summarize/gFlowSummarize.js
+++ summarize/gFlowSummarize.js
@@ -51,6 +51,7 @@ function summarize(article) {
                 article.summary = summary.trim();
                 resolve(article);
             }
+            tmpDir.removeCallback();
         });
     });
 }
