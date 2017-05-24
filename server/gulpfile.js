377f418607073ba2a5b877d8e4839f919085e3cc
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2017-05-24 14:53:58 +0530

    [WIP] Workbench

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,15 +1,36 @@
 var gulp = require('gulp');
+var del = require('del');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 
+var path = {
+    css: {
+        src: 'static/css/src/*.css',
+        dest: 'static/css/build'
+    },
+};
+
+gulp.task('clean', function() {
+    // Must be synchronous if we're going to use this task as a dependency
+    for (var taskPath in path) {
+        if (path.hasOwnProperty(taskPath)) {
+            del.sync(path[taskPath].dest + '/');
+        }
+    }
+});
+
 gulp.task('css', function() {
-    return gulp.src('static/css/src/*.css')
+    return gulp.src(path.css.src)
         .pipe(postcss(
             [
                 autoprefixer({ cascade: false })
             ]
         ))
-        .pipe(gulp.dest('static/css'));
+        .pipe(gulp.dest(path.css.dest));
+});
+
+gulp.task('watch', ['default'], function() {
+    gulp.watch(path.css.src, ['css']);
 });
 
 gulp.task('default', ['css']);
