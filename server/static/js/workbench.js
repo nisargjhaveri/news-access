efe76810fd8d7cc78a31cb974ef7d63426df20d4
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2017-06-13 13:50:22 +0530

    Make summary editable along with sortable

--- server/static/js/workbench.js
+++ server/static/js/workbench.js
@@ -9,7 +9,6 @@ function prepareArticle() {
 
             $('<p class="paragraph paragraph-draggable paragraph-source flex-item">')
                 .append($('<span class="sentence">').text(sentences[i].source))
-                .append($('<span class="sentence">').text(sentences[i].source))
                 .appendTo(para);
             $('<p class="paragraph flex-item">')
                 .append($('<span class="sentence">').text(sentences[i].target))
@@ -25,6 +24,41 @@ function prepareArticle() {
             .append($('<span class="sentence">').text(
                 "A two-time Rajya Sabha MP from Madhya Pradesh, Dave was known in the BJP circles as a man gifted with immaculate organisational skills."
             ));
+
+        $('.summary-source')
+            .on('click', '.sentence', function(e) {
+                // Do nothing if already contenteditable
+                if ($(this).attr('contenteditable')) {
+                    return;
+                }
+
+                $(this).attr('contenteditable', true);
+
+                // Focus and set caret position
+                var range;
+                var textNode;
+                var offset;
+
+                if (document.caretPositionFromPoint) {
+                    range = document.caretPositionFromPoint(e.clientX, e.clientY);
+                    textNode = range.offsetNode;
+                    offset = range.offset;
+                } else if (document.caretRangeFromPoint) {
+                    range = document.caretRangeFromPoint(e.clientX, e.clientY);
+                    textNode = range.startContainer;
+                    offset = range.startOffset;
+                }
+
+                range = document.createRange();
+                var sel = window.getSelection();
+                range.setStart(textNode, offset);
+                range.collapse(true);
+                sel.removeAllRanges();
+                sel.addRange(range);
+            })
+            .on("blur", '.sentence', function(e) {
+                $(this).removeAttr('contenteditable');
+            });
     }
 
     function enableDragDrop() {
@@ -39,28 +73,35 @@ function prepareArticle() {
             });
         });
 
-        Sortable.create($('.summary-source').get(0), {
-            group: {
-                name: "summary-source",
-                pull: true,
-                put: ["source-sentence-list"]
-            },
-            sort: true,
-            onStart: function(e) {
-                $('.summary-bin-container').removeClass('hidden');
-            },
-            onEnd: function(e) {
-                $('.summary-bin-container').addClass('hidden');
-                $('.summary-bin').removeClass('summary-bin-highlight');
-            },
-            onMove: function (e) {
-                if ($(e.to).is(".summary-bin")) {
-                    $('.summary-bin').addClass('summary-bin-highlight');
-                } else {
+        $('.summary-source').data(
+            'sortable',
+            Sortable.create($('.summary-source').get(0), {
+                group: {
+                    name: "summary-source",
+                    pull: true,
+                    put: ["source-sentence-list"]
+                },
+                sort: true,
+                preventOnFilter: false,
+                filter: function(e, target) {
+                    return $(target).attr('contenteditable');
+                },
+                onStart: function(e) {
+                    $('.summary-bin-container').removeClass('hidden');
+                },
+                onEnd: function(e) {
+                    $('.summary-bin-container').addClass('hidden');
                     $('.summary-bin').removeClass('summary-bin-highlight');
+                },
+                onMove: function (e) {
+                    if ($(e.to).is(".summary-bin")) {
+                        $('.summary-bin').addClass('summary-bin-highlight');
+                    } else {
+                        $('.summary-bin').removeClass('summary-bin-highlight');
+                    }
                 }
-            }
-        });
+            })
+        );
 
         Sortable.create($('.summary-bin').get(0), {
             group: {
